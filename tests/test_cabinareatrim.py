daf3199bbb939c4cab6b0d30990d75028d2b8811
Semen Filippov <semenfilippov@gmail.com>
2022-05-04 17:38:35 +0300

    added test which compares calc result to real data

--- tests/test_cabinareatrim.py
+++ tests/test_cabinareatrim.py
@@ -4,18 +4,19 @@ from crjwb.cabinareatrim import calc_pax_influence, calc_pax_weight
 AVG_ADULT_WEIGHT = 75
 AVG_CHILD_WEIGHT = 30
 AVG_INFANT_WEIGHT = 15
-NUM_ADULTS = 40
+NUM_ADULTS = 41
 NUM_CHILDREN = 2
-NUM_INFANTS = 1
-CABIN_BAGGAGE = 150
+NUM_INFANTS = 0
+CABIN_BAGGAGE = 91
 A_INFLUENCE = -0.01997
 B_INFLUENCE = -0.01013
 C_INFLUENCE = -0.00161
 D_INFLUENCE = 0.00627
-A_PAX = 11
-B_PAX = 11
-C_PAX = 10
+A_PAX = 12
+B_PAX = 12
+C_PAX = 9
 D_PAX = 10
+REAL_PAX_INFLUENCE = -(49.81 + 181 * 0.01547 - 29.84)
 
 
 def test_calc_pax_weight():
@@ -29,7 +30,7 @@ def test_calc_pax_weight():
             NUM_INFANTS,
             CABIN_BAGGAGE,
         )
-        == 3225
+        == 3226
     )
 
 
@@ -44,4 +45,24 @@ def test_calc_pax_influence():
         B_PAX,
         C_PAX,
         D_PAX,
-    ) == pytest.approx(-21.3375, 1e-4)
+    ) == pytest.approx(-23.474, 1e-3)
+
+
+def test_compare_to_real_calculation():
+    assert (
+        abs(
+            calc_pax_influence(
+                AVG_ADULT_WEIGHT,
+                A_INFLUENCE,
+                B_INFLUENCE,
+                C_INFLUENCE,
+                D_INFLUENCE,
+                A_PAX,
+                B_PAX,
+                C_PAX,
+                D_PAX,
+            )
+            - REAL_PAX_INFLUENCE
+        )
+        < 1
+    )
