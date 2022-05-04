9ee665d469d00470317435c103feb56557000131
Semen Filippov <semenfilippov@gmail.com>
2022-05-04 13:41:22 +0300

    docstring edit

--- crjwb/stab.py
+++ crjwb/stab.py
@@ -1,25 +1,34 @@
-def calc_stab(mac: float, eicas_formatted: bool = False) -> float:
+def calc_stab(mactow: float, eicas_formatted: bool = False) -> float:
     """
-    Get stab trim setting for given %MAC.\n
-    The function is derived from the fact that:\n
-    MAC 8.8 => STAB 8.15\n
-    MAC 35  => STAB 4.00\n
-    Therefore:\n
-    f(x) = -0.158397x + 9.54389\n
+    Get takeoff stab trim setting for given %MAC.
+
+    The function is derived from the fact that:
+
+    MAC 8.8 => STAB 8.15
+
+    MAC 35  => STAB 4.00
+
+    Therefore:
+
+    f(x) = -0.158397x + 9.54389
+
     Args:
-        mac (float): %MAC
-        eicas_formatted (bool, optional): Whether to round first decimal to even number.
+        ``mactow`` (float): %MAC for takeoff
+        ``eicas_formatted`` (bool, optional): Whether to round first decimal \
+        to even number.
         Defaults to False.
 
     Raises:
-        ValueError: If provided %MAC is out of bounds (8.8-35.0)
+        ``ValueError``: If provided ``mactow`` is out of range (8.8-35.0)
 
     Returns:
-        float: stabilizer trim setting
+        ``float``: stabilizer trim setting
     """
-    if mac < 8.8 or mac > 35:
-        raise ValueError(f"MAC {mac} is out of bounds. Should be within 8.8-35.0 %MAC.")
-    stab = mac * -0.158397 + 9.54389
+    if mactow < 8.8 or mactow > 35:
+        raise ValueError(
+            f"MAC {mactow} is out of range. Should be within 8.8-35.0 %MAC."
+        )
+    stab = mactow * -0.158397 + 9.54389
     if eicas_formatted:
         return round(stab / 2, 1) * 2
     else:
