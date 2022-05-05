b2c7d6b83ea90ee20b134ff6e6081ec701b98f2a
Semen Filippov <semenfilippov@gmail.com>
2022-05-05 13:01:29 +0300

    updated imports

--- crjwb/__init__.py
+++ crjwb/__init__.py
@@ -1,3 +1,27 @@
 """
 Weight and balance calculation module.
 """
+from crjwb.calculator import CalculationResult, calculate_wb
+from crjwb.exceptions import (
+    AftMACLimitsViolatedError,
+    ForwardMACLimitsViolatedError,
+    IncorrectTripFuelError,
+    NotEnoughSeatsOccupiedError,
+    PayloadTooHeavyError,
+    TooManySeatsOccupiedError,
+)
+from crjwb.inputclasses import AircraftData, CalculationTask, StandardWeights
+
+__all__ = [
+    "CalculationResult",
+    "calculate_wb",
+    "AftMACLimitsViolatedError",
+    "ForwardMACLimitsViolatedError",
+    "IncorrectTripFuelError",
+    "NotEnoughSeatsOccupiedError",
+    "PayloadTooHeavyError",
+    "TooManySeatsOccupiedError",
+    "AircraftData",
+    "CalculationTask",
+    "StandardWeights",
+]
