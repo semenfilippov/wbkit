8a9d4c4f27fdd450224ceb4a9f4d636c55496b91
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-12 18:51:58 +0300

    removed constant values from basic.py, created BasicCalc class

--- src/crjwb/basic.py
+++ src/crjwb/basic.py
@@ -1,42 +1,57 @@
-# Reference station/axis. Selected station around which all index values are calculated.
-REF_STATION = 13.2  # meters from zero
-# Constant used as a plus value to avoid negative index figures.
-K_CONSTANT = 50
-# Constant used as a denominator to convert moment values into index values.
-C_CONSTANT = 280
+class BasicCalc:
+    def __init__(
+        self,
+        ref_station: float,
+        k_constant: int,
+        c_constant: int,
+        macrc_length: float,
+        lemac_at: float,
+    ) -> None:
+        """Initialize BasicCalc.
 
-# MAC/RC formula constants.
-# Length of Mean Aerodynamic Chord / Reference Chord in length units.
-MACRC_LENGTH = 2.526  # meters
-# Horizontal distance in length units from the station zero
-# to location of the Leading Edge of the MAC / RC
-LEMAC_AT = 12.542  # meters from zero
+        Args:
+            ``ref_station`` (float): Reference station/axis. Selected station \
+                around which all index values are calculated.
+            ``k_constant`` (int): Constant used as a plus value to avoid \
+                negative index figures.
+            ``c_constant`` (int): Constant used as a denominator to convert \
+                moment values into index values.
+            ``macrc_length`` (float): Length of \
+                Mean Aerodynamic Chord / Reference Chord in length units.
+            ``lemac_at`` (float): Horizontal distance in length units \
+                from the station zero to location of the Leading Edge of the MAC / RC.
+        """
+        self.ref_station = ref_station
+        self.k_constant = k_constant
+        self.c_constant = c_constant
+        self.macrc_length = macrc_length
+        self.lemac_at = lemac_at
 
+    def calc_index(self, weight: float, station: float) -> float:
+        """Calculate index.
+        Args:
+            ``weight`` (float): actual weight
+            ``station`` (float): horizontal distance in meters
+            from station zero to the location
+        Returns:
+            ``float``: index
+        """
+        return float(
+            (weight * (station - self.ref_station)) / self.c_constant + self.k_constant
+        )
 
-# def calc_index(weight: float, station: float) -> float:
-#    """Calculate index.
-#
-#    Args:
-#        weight (float): actual weight
-#        station (float): horizontal distance in meters
-#        from station zero to the location
-#
-#    Returns:
-#        float: index
-#    """
-#    return float((weight * (station - REF_STATION)) / C_CONSTANT + K_CONSTANT)
+    def calc_macrc(self, idx: float, weight: float) -> float:
+        """Calculate MAC/RC.
 
+        Args:
+            ``idx`` (float): index value corresponding to respective weight
+            ``weight`` (float): actual weight
 
-def calc_macrc(idx: float, weight: float) -> float:
-    """Calculate MAC/RC.
-
-    Args:
-        idx (float): index value corresponding to respective weight
-        weight (float): actual weight
-
-    Returns:
-        float: MAC/RC
-    """
-    return (((C_CONSTANT * (idx - K_CONSTANT)) / weight) + REF_STATION - LEMAC_AT) / (
-        MACRC_LENGTH / 100
-    )
+        Returns:
+            ``float``: MAC/RC
+        """
+        return (
+            ((self.c_constant * (idx - self.k_constant)) / weight)
+            + self.ref_station
+            - self.lemac_at
+        ) / (self.macrc_length / 100)
--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -1,10 +1,41 @@
 import pytest
-from crjwb.basic import calc_macrc  # noqa
-
-ZFW = 17843
-LIZFW = 29.84
-MACZFW = 13.52
+from crjwb.basic import BasicCalc
 
 
-def test_calc_macrc():
-    assert calc_macrc(LIZFW, ZFW) == pytest.approx(MACZFW, 1e-3)
+@pytest.fixture(scope="module")
+def basic_calc():
+    """
+    Get BasicCalc with following parameters:
+
+    REF_STATION = 13.2
+
+    K_CONSTANT = 50
+
+    C_CONSTANT = 280
+
+    MACRC_LENGTH = 2.526
+
+    LEMAC_AT = 12.542
+
+
+    Yields:
+        BasicCalc: BasicCalc
+    """
+    yield BasicCalc(13.2, 50, 280, 2.526, 12.542)
+
+
+@pytest.mark.parametrize(
+    "basic_calc, idx, weight, mac",
+    [
+        ("basic_calc", 29.84, 17843, 13.52),
+        ("basic_calc", 23.72, 21627, 12.58),
+        ("basic_calc", 23.83, 20354, 11.80),
+    ],
+    indirect=["basic_calc"],
+)
+def test_calc_macrc(basic_calc, idx, weight, mac):
+    """
+    Compare BasicCalc.calc_macrc() calculations results with \
+    real loadsheet data.
+    """
+    assert basic_calc.calc_macrc(idx, weight) == pytest.approx(mac, 1e-3)
