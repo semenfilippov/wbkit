9550bad0f8eba2d2956bf4d55711e71d94d8d0b1
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-12 23:47:55 +0300

    detached preset fuel indexes from fuel module, created FuelEffect class

--- src/crjwb/fuel.py
+++ src/crjwb/fuel.py
@@ -1,75 +1,49 @@
-FUEL_INDEX_INFLUENCE = {
-    200: -0.83,
-    400: -1.56,
-    600: -2.22,
-    800: -2.83,
-    1000: -3.40,
-    1200: -3.92,
-    1400: -4.39,
-    1600: -4.81,
-    1800: -5.17,
-    2000: -5.47,
-    2200: -5.73,
-    2400: -5.93,
-    2600: -6.07,
-    2800: -6.21,
-    3000: -6.32,
-    3200: -6.31,
-    3400: -6.27,
-    3600: -6.27,
-    3800: -6.11,
-    4000: -6.02,
-    4200: -5.60,
-    4255: -5.47,
-    4400: -6.30,
-    4600: -7.38,
-    4800: -8.40,
-    5000: -9.44,
-    5200: -10.56,
-    5400: -11.56,
-    5600: -12.63,
-    5800: -13.69,
-    6000: -14.82,
-    6200: -15.98,
-    6400: -17.28,
-    6488: -17.91,
-}
-MAX_FUEL = max(FUEL_INDEX_INFLUENCE)
+from typing import Dict
 
 
-def get_closest_fuel_index(fuel: int) -> float:
-    """Get closest fuel index.
+class FuelEffect:
+    def __init__(self, fuel_effect: Dict[int, float]) -> None:
+        if not fuel_effect:
+            raise ValueError(
+                "Argument fuel_effect must constist at least of one key value pair"
+            )
+        self.fuel_effect = fuel_effect
+        self.min_fuel = min(self.fuel_effect)
+        self.max_fuel = max(self.fuel_effect)
 
-    Args:
-        fuel (int): fuel quantity
+    def get_influence(self, fuel: int) -> float:
+        """Get closest fuel index.
 
-    Raises:
-        ValueError: if fuel value is negative
-        ValueError: if fuel value exceeds maximum
+        Args:
+            fuel (int): fuel quantity
 
-    Returns:
-        float: index influence
-    """
-    if fuel < 0:
-        raise ValueError(f"Incorrect fuel value: {fuel} (must not be negative).")
-    if fuel in FUEL_INDEX_INFLUENCE:
-        return FUEL_INDEX_INFLUENCE[fuel]
-    if fuel > MAX_FUEL:
-        raise ValueError(f"Incorrect fuel value: {fuel} (must not exceed {MAX_FUEL}).")
-    closest_keys = {abs(fuel - x) for x in FUEL_INDEX_INFLUENCE}
-    min_diff = min(closest_keys)
-    if (
-        fuel + min_diff in FUEL_INDEX_INFLUENCE
-        and fuel - min_diff in FUEL_INDEX_INFLUENCE
-    ):
-        more_fuel_index = FUEL_INDEX_INFLUENCE[fuel + min_diff]
-        less_fuel_index = FUEL_INDEX_INFLUENCE[fuel - min_diff]
-        return (
-            more_fuel_index
-            if abs(more_fuel_index) >= abs(less_fuel_index)
-            else less_fuel_index
-        )
-    if fuel + min_diff in FUEL_INDEX_INFLUENCE:
-        return FUEL_INDEX_INFLUENCE[fuel + min_diff]
-    else:
-        return FUEL_INDEX_INFLUENCE[fuel - min_diff]
+        Raises:
+            ValueError: if fuel value is negative
+            ValueError: if fuel value exceeds maximum
+
+        Returns:
+            float: index influence
+        """
+        if fuel in self.fuel_effect:
+            return self.fuel_effect[fuel]
+        if fuel < self.min_fuel:
+            raise ValueError(
+                f"Incorrect fuel value: {fuel} (must not be less than {self.min_fuel})."
+            )
+        if fuel > self.max_fuel:
+            raise ValueError(
+                f"Incorrect fuel value: {fuel} (must not exceed {self.max_fuel})."
+            )
+        min_diff = min({abs(fuel - x) for x in self.fuel_effect})
+        possible_indexes = [
+            self.fuel_effect[x]
+            for x in {fuel + min_diff, fuel - min_diff}
+            if x in self.fuel_effect
+        ]
+        if len(possible_indexes) > 1:
+            return (
+                possible_indexes[0]
+                if abs(possible_indexes[0]) >= abs(possible_indexes[1])
+                else possible_indexes[1]
+            )
+        return possible_indexes[0]
--- src/tests/test_fuel.py
+++ src/tests/test_fuel.py
@@ -1,28 +1,78 @@
 import pytest
-from crjwb.fuel import get_closest_fuel_index
+from crjwb.fuel import FuelEffect
 
 
-def test_negative_fuel():
+@pytest.fixture(scope="module")
+def fe_dict() -> dict:
+    return {
+        200: -0.83,
+        400: -1.56,
+        600: -2.22,
+        800: -2.83,
+        1000: -3.40,
+        1200: -3.92,
+        1400: -4.39,
+        1600: -4.81,
+        1800: -5.17,
+        2000: -5.47,
+        2200: -5.73,
+        2400: -5.93,
+        2600: -6.07,
+        2800: -6.21,
+        3000: -6.32,
+        3200: -6.31,
+        3400: -6.27,
+        3600: -6.27,
+        3800: -6.11,
+        4000: -6.02,
+        4200: -5.60,
+        4255: -5.47,
+        4400: -6.30,
+        4600: -7.38,
+        4800: -8.40,
+        5000: -9.44,
+        5200: -10.56,
+        5400: -11.56,
+        5600: -12.63,
+        5800: -13.69,
+        6000: -14.82,
+        6200: -15.98,
+        6400: -17.28,
+        6488: -17.91,
+    }
+
+
+@pytest.fixture(scope="module")
+def fe_obj(fe_dict: dict) -> FuelEffect:
+    yield FuelEffect(fe_dict)
+
+
+def test_empty_dict():
     with pytest.raises(ValueError):
-        get_closest_fuel_index(-1)
+        FuelEffect({})
 
 
-def test_fuel_exceeds_maximum():
+def test_get_less_than_min(fe_obj: FuelEffect):
     with pytest.raises(ValueError):
-        get_closest_fuel_index(6500)
+        fe_obj.get_influence(100)
 
 
-def test_exact_fuel_key():
-    assert get_closest_fuel_index(200) == -0.83
+def test_get_greater_than_max(fe_obj: FuelEffect):
+    with pytest.raises(ValueError):
+        fe_obj.get_influence(6500)
 
 
-def test_return_less():
-    assert get_closest_fuel_index(3250) == -6.31
+def test_get_exact(fe_obj: FuelEffect):
+    assert fe_obj.get_influence(3000) == -6.32
 
 
-def test_return_more():
-    assert get_closest_fuel_index(3350) == -6.27
+def test_get_approx_less(fe_obj: FuelEffect):
+    assert fe_obj.get_influence(6299) == -15.98
 
 
-def test_equidistant_fuel_returns_greater_influence():
-    assert get_closest_fuel_index(3300) == -6.31
+def test_get_approx_greater(fe_obj: FuelEffect):
+    assert fe_obj.get_influence(6301) == -17.28
+
+
+def test_get_equidistant(fe_obj: FuelEffect):
+    assert fe_obj.get_influence(6300) == -17.28
