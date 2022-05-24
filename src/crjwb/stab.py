134083c6e7d48bb6bdb4dbb992c3088b98c7ef05
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-24 16:06:12 +0300

    class Stab is now a subclass to Interpolatable

--- src/crjwb/stab.py
+++ src/crjwb/stab.py
@@ -1,44 +1,27 @@
 from typing import Dict, Union
-import numpy as np
+
+from crjwb.basic import Interpolatable
 
 
-class Stab:
+class Stab(Interpolatable):
     """Stabilizer trim calculator"""
 
-    def __init__(self, points: Dict[Union[int, float], Union[int, float]]) -> None:
+    def __init__(self, points: Dict[int, Union[int, float]]) -> None:
         """Get new stabilizer trim calculator.
 
-        Parameters
-        ----------
-        ``points`` (Dict[int | float, int | float]): stab trim function points,
-        where keys are MAC/RC values, values are corresponding stab trim settings
-
-        Raises
-        ----------
-        ValueError: if ``points`` length is less than 2
+        Args:
+            points (Dict[int, Union[int, float]]): stab trim function points,
+            where keys are MAC/RC values, values are corresponding stab trim settings
         """
-        if len(points) < 2:
-            raise ValueError("You should provide at least two points!")
-        self.xp = sorted([x for x in points])
-        self.xp_min = min(self.xp)
-        self.xp_max = max(self.xp)
-        self.fp = [points[x] for x in self.xp]
+        super().__init__(points)
 
     def calc(self, mactow: float) -> float:
         """Get takeoff stab trim setting for given %MAC.
 
-        Parameters
-        ----------
-        mactow (float): %MAC/RC for TOW
+        Args:
+            mactow (float): %MAC/RC for TOW
 
-        Raises
-        ----------
-        ValueError: if mactow is outside of defined range
-
-        Returns
-        ----------
-        float: stab trim setting
+        Returns:
+            float: stab trim setting
         """
-        if mactow < self.xp_min or mactow > self.xp_max:
-            raise ValueError(f"MACTOW should be within {self.xp_min}% - {self.xp_max}%")
-        return float(np.interp(mactow, self.xp, self.fp))
+        return self.get_interpolated_value(mactow)
