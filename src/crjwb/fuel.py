4b1e56ce433684b5f1f5bbfbdf4fff79ba888bfd
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-24 16:17:59 +0300

    FuelEffect is now a subclass of Interpolatable

--- src/crjwb/fuel.py
+++ src/crjwb/fuel.py
@@ -1,83 +1,29 @@
 from typing import Dict, Union
+from crjwb.basic import Interpolatable
 
 
-class FuelEffect:
-    def __init__(self, fuel_effect: Dict[int, Union[int, float]]) -> None:
-        """Construct new FuelEffect object.
+class FuelEffect(Interpolatable):
+    def __init__(self, points: Dict[int, Union[int, float]]) -> None:
+        """Get new FuelEffect object.
 
-        Parameters
-        ----------
-        ``fuel_effect`` (Dict[int, int | float]]): keys are fuel quantities,
-        values are their index influencies
-
-        Raises
-        ----------
-        ValueError: if fuel_effect is empty
+        Args:
+            points (Dict[int, Union[int, float]]): fuel effect index influence points,
+            where key is fuel quantity, value is index influence
         """
-        if not fuel_effect:
-            raise ValueError(
-                "Argument fuel_effect must constist at least of one key value pair"
-            )
-        self.fuel_effect = fuel_effect
-        self.min_fuel = min(self.fuel_effect)
-        self.max_fuel = max(self.fuel_effect)
+        super().__init__(points)
 
-    def get_min_fuel(self) -> int:
-        """Get minimum fuel quantity FuelEffect object is able
-        to give index influence for.
+    def get_influence(self, fuel: int, allow_interpolation: bool = False) -> float:
+        """Get index influence for given fuel quantity.
 
-        Returns
-        ----------
-        int: fuel quantity
+        Args:
+            `fuel` (int): fuel quantity
+            `allow_interpolation` (bool, optional): Whether to interpolate
+            index influence or not.
+            Defaults to False.
+
+        Returns:
+            `float`: index influence
         """
-        return self.min_fuel
-
-    def get_max_fuel(self) -> int:
-        """Get maximum fuel quantity FuelEffect object is able
-        to give index influence for.
-
-        Returns
-        ----------
-        int: fuel quantity
-        """
-        return self.max_fuel
-
-    def get_influence(self, fuel: int) -> float:
-        """Get closest fuel index.
-
-        Parameters
-        ----------
-        fuel (int): fuel quantity
-
-        Raises
-        ----------
-        ValueError: if fuel value is negative
-        ValueError: if fuel value exceeds maximum
-
-        Returns
-        ----------
-        float: index influence
-        """
-        if fuel in self.fuel_effect:
-            return self.fuel_effect[fuel]
-        if fuel < self.min_fuel:
-            raise ValueError(
-                f"Incorrect fuel value: {fuel} (must not be less than {self.min_fuel})."
-            )
-        if fuel > self.max_fuel:
-            raise ValueError(
-                f"Incorrect fuel value: {fuel} (must not exceed {self.max_fuel})."
-            )
-        min_diff = min({abs(fuel - x) for x in self.fuel_effect})
-        possible_indexes = [
-            self.fuel_effect[x]
-            for x in {fuel + min_diff, fuel - min_diff}
-            if x in self.fuel_effect
-        ]
-        if len(possible_indexes) > 1:
-            return (
-                possible_indexes[0]
-                if abs(possible_indexes[0]) >= abs(possible_indexes[1])
-                else possible_indexes[1]
-            )
-        return possible_indexes[0]
+        if allow_interpolation:
+            return self.get_interpolated_value(fuel)
+        return self.get_defined_value(fuel)
