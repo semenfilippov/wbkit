c5056a3f8fad6196369c1dd48f90c804da76dc42
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-24 15:55:27 +0300

    created Interpolatable parent class

--- src/crjwb/basic.py
+++ src/crjwb/basic.py
@@ -1,3 +1,7 @@
+from typing import Dict, Union
+import numpy as np
+
+
 class BasicCalc:
     def __init__(
         self,
@@ -10,15 +14,15 @@ class BasicCalc:
         """Initialize BasicCalc.
 
         Args:
-            ``ref_station`` (float): Reference station/axis. Selected station \
+            `ref_station` (float): Reference station/axis. Selected station \
                 around which all index values are calculated.
-            ``k_constant`` (int): Constant used as a plus value to avoid \
+            `k_constant` (int): Constant used as a plus value to avoid \
                 negative index figures.
-            ``c_constant`` (int): Constant used as a denominator to convert \
+            `c_constant` (int): Constant used as a denominator to convert \
                 moment values into index values.
-            ``macrc_length`` (float): Length of \
+            `macrc_length` (float): Length of \
                 Mean Aerodynamic Chord / Reference Chord in length units.
-            ``lemac_at`` (float): Horizontal distance in length units \
+            `lemac_at` (float): Horizontal distance in length units \
                 from the station zero to location of the Leading Edge of the MAC / RC.
         """
         self.ref_station = ref_station
@@ -30,15 +34,13 @@ class BasicCalc:
     def calc_index(self, weight: float, station: float) -> float:
         """Calculate index.
 
-        Parameters
-        ----------
-        ``weight`` (float): actual weight
-        ``station`` (float): horizontal distance in meters
+        Args:
+            `weight` (float): actual weight
+            `station` (float): horizontal distance in meters
         from station zero to the location
 
-        Returns
-        ----------
-        ``float``: index
+        Returns:
+            `float`: index
         """
         return float(
             (weight * (station - self.ref_station)) / self.c_constant + self.k_constant
@@ -47,17 +49,114 @@ class BasicCalc:
     def calc_macrc(self, idx: float, weight: float) -> float:
         """Calculate MAC/RC.
 
-        Parameters
-        ----------
-        ``idx`` (float): index value corresponding to respective weight
-        ``weight`` (float): actual weight
+        Args:
+            `idx` (float): index value corresponding to respective weight
+            `weight` (float): actual weight
 
-        Returns
-        ----------
-        ``float``: MAC/RC
+        Returns:
+            `float`: MAC/RC
         """
         return (
             ((self.c_constant * (idx - self.k_constant)) / weight)
             + self.ref_station
             - self.lemac_at
         ) / (self.macrc_length / 100)
+
+
+class Interpolatable:
+    def __init__(self, points: Dict[int, Union[float, int]]) -> None:
+        """Create Interpolatable object.
+
+        Args:
+            `points` (Dict[int, Union[float, int]]): {x: f(x)} dict
+            order of x values may be arbitrary
+
+        Raises:
+            `ValueError`: if len(points) < 2
+        """
+        if len(points) < 2:
+            raise ValueError(
+                "You should provide at least two points "
+                "to construct Interpolateble object!"
+            )
+        self.__points__ = points
+        self.__xp__ = sorted([x for x in self.__points__])
+        self.__fp__ = [points[x] for x in self.__xp__]
+        self.__min_x__ = min(self.__xp__)
+        self.__max_x__ = max(self.__xp__)
+
+    def get_min(self) -> int:
+        """Get minumum x value of defined x range.
+
+        Returns:
+            `int`: min(x)
+        """
+        return self.__min_x__
+
+    def get_max(self) -> int:
+        """Get maximum x value of defined x range.
+
+        Returns:
+            `int`: max(x)
+        """
+        return self.__max_x__
+
+    def __validate_in_range__(self, x: Union[float, int]):
+        """This method is intended for internal use to validate if
+        given `x` is in defined x range.
+
+        Args:
+            `x` (Union[float, int]): x value to validate
+
+        Raises:
+            `ValueError`: if `x` is not within Interpolateble object x range
+        """
+        if x < self.__min_x__:
+            raise ValueError(f"x is out of range, should be >= {self.__min_x__}")
+        if x > self.__max_x__:
+            raise ValueError(f"x is out of range, should be <= {self.__max_x__}")
+
+    def get_interpolated_value(self, x: Union[float, int]) -> float:
+        """Get interpolated f(x).
+
+        Args:
+            `x` (Union[float, int]): x value to interpolate
+
+        Returns:
+            `float`: interpolated f(x)
+        """
+        self.__validate_in_range__(x)
+        return float(np.interp(x, self.__xp__, self.__fp__))
+
+    def get_defined_value(self, x: int) -> Union[float, int]:
+        """Get nearest defined f(x). The logic of this method is as follows.
+
+        If `x` is found among `self.points` keys, return its value.
+        If not, calculate minimal distance between given x
+        and each key in `self.points`. If there is more than one value
+        for `self.points[x+-difference]`, return the one which
+        has greater absolute value.
+
+
+        Args:
+            `x` (int): x value
+
+        Returns:
+            `Union[float, int]`: nearest defined f(x)
+        """
+        self.__validate_in_range__(x)
+        if x in self.__points__:
+            return self.__points__[x]
+        min_diff = min({abs(x - f) for f in self.__points__})
+        possible_values = [
+            self.__points__[xd]
+            for xd in {x + min_diff, x - min_diff}
+            if xd in self.__points__
+        ]
+        if len(possible_values) > 1:
+            return (
+                possible_values[0]
+                if abs(possible_values[0]) >= abs(possible_values[1])
+                else possible_values[1]
+            )
+        return possible_values[0]
--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -1,9 +1,9 @@
 import pytest
-from crjwb.basic import BasicCalc
+from crjwb.basic import BasicCalc, Interpolatable
 
 
 @pytest.fixture(scope="module")
-def basic_calc():
+def basic_calc() -> BasicCalc:
     """
     Get BasicCalc with following parameters:
 
@@ -21,7 +21,12 @@ def basic_calc():
     Yields:
         BasicCalc: BasicCalc
     """
-    yield BasicCalc(13.2, 50, 280, 2.526, 12.542)
+    return BasicCalc(13.2, 50, 280, 2.526, 12.542)
+
+
+@pytest.fixture(scope="module")
+def interp() -> Interpolatable:
+    return Interpolatable({1: 10, 2: 20, 3: 30, 40: 400, 50: -500})
 
 
 @pytest.mark.parametrize(
@@ -39,3 +44,58 @@ def test_calc_macrc(basic_calc, idx, weight, mac):
     real loadsheet data.
     """
     assert basic_calc.calc_macrc(idx, weight) == pytest.approx(mac, 1e-3)
+
+
+def test_interp_one_point_raises():
+    with pytest.raises(ValueError):
+        Interpolatable({1: 10})
+
+
+def test_range_validation_does_not_raise(interp: Interpolatable):
+    interp.__validate_in_range__(1.5)
+
+
+def test_range_validation_lt_min_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.__validate_in_range__(0.9)
+
+
+def test_range_validation_gt_max_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.__validate_in_range__(51)
+
+
+def test_interp_lt_min_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.get_interpolated_value(0.5)
+
+
+def test_interp_gt_max_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.get_interpolated_value(51)
+
+
+def test_interpolate_value(interp: Interpolatable):
+    assert interp.get_interpolated_value(1.5) == 15
+
+
+def test_defined_lt_min_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.get_defined_value(0)
+
+
+def test_defined_gt_max_raises(interp: Interpolatable):
+    with pytest.raises(ValueError):
+        interp.get_defined_value(51)
+
+
+def test_defined_less(interp: Interpolatable):
+    assert interp.get_defined_value(44) == 400
+
+
+def test_defined_greater(interp: Interpolatable):
+    assert interp.get_defined_value(46) == -500
+
+
+def test_defined_equidistant(interp: Interpolatable):
+    assert interp.get_defined_value(45) == -500
