fd82f6566aa6e41718bb6e78d67b5cc643388fd3
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-27 12:23:33 +0300

    substituted BasicCalc with Index and PercentMAC classes

--- src/crjwb/basic.py
+++ src/crjwb/basic.py
@@ -1,59 +1,191 @@
-class BasicCalc:
-    def __init__(
-        self,
-        ref_station: float,
-        k_constant: int,
-        c_constant: int,
-        macrc_length: float,
-        lemac_at: float,
-    ) -> None:
-        """Initialize BasicCalc.
+class Index:
+    """Moment index representation."""
+
+    def __init__(self, idx: float, weight: int, ref_st: float, c: int, k: int) -> None:
+        """Create new Index object.
 
         Args:
-            `ref_station` (float): Reference station/axis. Selected station
-            around which all index values are calculated.
-            `k_constant` (int): Constant used as a plus value to avoid
-            negative index figures.
-            `c_constant` (int): Constant used as a denominator to convert
-            moment values into index values.
-            `macrc_length` (float): Length of
-            Mean Aerodynamic Chord / Reference Chord in length units.
-            `lemac_at` (float): Horizontal distance in length units
-            from the station zero to location of the Leading Edge of the MAC / RC.
+            idx (float): Index value
+            weight (int): Corresponding weight
+            ref_st (float): Reference station/axis. Selected station
+            around which all index values are calculated
+            c (int): Constant used as a denominator to convert
+            moment values into index values
+            k (int): Constant used as a plus value to avoid
+            negative index figures
+
+        Raises:
+            ValueError: if `c` equals to 0
         """
-        self.ref_station = ref_station
-        self.k_constant = k_constant
-        self.c_constant = c_constant
-        self.macrc_length = macrc_length
-        self.lemac_at = lemac_at
+        if not c > 0:
+            raise ValueError("C constant should be greater than 0")
+        if k < 0:
+            raise ValueError("K constant should not be negative")
+        self.value = idx
+        self.weight = weight
+        self.ref_st = ref_st
+        self.c = c
+        self.k = k
 
-    def calc_index(self, weight: float, station: float) -> float:
-        """Calculate index.
-
-        Args:
-            `weight` (float): actual weight
-            `station` (float): horizontal distance in meters
-        from station zero to the location
+    @property
+    def moment(self) -> float:
+        """Get moment value.
 
         Returns:
-            `float`: index
+            float: moment
         """
-        return float(
-            (weight * (station - self.ref_station)) / self.c_constant + self.k_constant
+        return (self.value - self.k) * self.c
+
+    @staticmethod
+    def calc(weight: int, station: float, ref_st: float, c: int, k: int):
+        """Calculate index value and return Index object.
+
+        Args:
+            weight (int): Actual weight
+            station (float): Horizontal distance in length units
+            from station zero to the location
+            ref_st (float): Reference station/axis. Selected station
+            around which all index values are calculated
+            c (int): Constant used as a denominator to convert
+            moment values into index values
+            k (int): Constant used as a plus value to avoid
+            negative index figures
+
+        Raises:
+            ValueError: if `c` equals to zero
+
+        Returns:
+            Index: calculated Index object
+        """
+        try:
+            idx = (weight * (station - ref_st)) / c + k
+        except ZeroDivisionError:
+            raise ValueError("C constant cannot be equal to 0")
+        return Index(idx, weight, ref_st, c, k)
+
+    def __validate_calc_ops__(self, other):
+        """Internal Index object validator.
+        Validate if two Index objects can be added or substracted.
+        It is only possible when their `ref_st`, `c` and `k`
+        attributes are equal.
+
+        Args:
+            other (Index): Index object
+
+        Raises:
+            ValueError: if either of `ref_st`, `c` or `k` Index object
+            attributes are not equal
+        """
+        if self.ref_st == other.ref_st and self.c == other.c and self.k == other.k:
+            return
+
+        raise ValueError(
+            "Calculations for Index operands with different "
+            "reference stations, C or K constants are not allowed."
         )
 
-    def calc_macrc(self, idx: float, weight: float) -> float:
-        """Calculate MAC/RC.
+    def __validate_compare_ops__(self, other):
+        """Internal Index object validator.
+        Validate if two Index objects can compared.
+        It is only possible when their `ref_st` attributes are equal.
 
         Args:
-            `idx` (float): index value corresponding to respective weight
-            `weight` (float): actual weight
+            other (Index): Index object
+
+        Raises:
+            ValueError: if `ref_st` attributes are not equal
+        """
+        if not self.ref_st == other.ref_st:
+            raise ValueError(
+                "Cannot compare Index instances with different reference stations."
+            )
+
+    def __add__(self, other):
+        self.__validate_calc_ops__(other)
+        sum_weights = self.weight + other.weight
+        sum_idxs = self.value + other.value
+        return Index(sum_idxs, sum_weights, self.ref_st, self.c, self.k)
+
+    def __sub__(self, other):
+        self.__validate_calc_ops__(other)
+        sum_weights = self.weight - other.weight
+        sum_idxs = self.value - other.value
+        return Index(sum_idxs, sum_weights, self.ref_st, self.c, self.k)
+
+    def __eq__(self, other):
+        self.__validate_compare_ops__(other)
+        return self.moment == other.moment
+
+    def __gt__(self, other):
+        self.__validate_compare_ops__(other)
+        return self.moment > other.moment
+
+    def __lt__(self, other):
+        self.__validate_compare_ops__(other)
+        return self.moment < other.moment
+
+    def __ge__(self, other):
+        self.__validate_compare_ops__(other)
+        return self > other or self == other
+
+    def __le__(self, other):
+        self.__validate_compare_ops__(other)
+        return self < other or self == other
+
+
+class PercentMAC:
+    """%MAC representation."""
+
+    def __init__(self, value: float, lemac_at: float, macrc_length: float) -> None:
+        """Create new PercentMAC object.
+
+        Args:
+            value (float): %MAC value
+            lemac_at (float): Horizontal distance in length units
+            from the station zero to location of the Leading Edge of the MAC / RC
+            macrc_length (float): Length of
+            Mean Aerodynamic Chord / Reference Chord in length units
+        """
+        self.value = value
+        self.lemac_at = lemac_at
+        self.macrc_length = macrc_length
+
+    @staticmethod
+    def from_idx(idx: Index, lemac_at: float, macrc_length: float):
+        """Create new PercentMAC object using Index object.
+
+        Args:
+            idx (Index): Index object
+            lemac_at (float): Horizontal distance in length units
+            from the station zero to location of the Leading Edge of the MAC / RC
+            macrc_length (float): Length of
+            Mean Aerodynamic Chord / Reference Chord in length units
 
         Returns:
-            `float`: MAC/RC
+            PercentMAC: PercentMAC object derived from Index object
         """
-        return (
-            ((self.c_constant * (idx - self.k_constant)) / weight)
-            + self.ref_station
-            - self.lemac_at
-        ) / (self.macrc_length / 100)
+        return PercentMAC(
+            ((idx.moment / idx.weight) + idx.ref_st - lemac_at) / (macrc_length / 100),
+            lemac_at,
+            macrc_length,
+        )
+
+    def to_idx(self, weight: int, ref_st: float, c: int, k: int):
+        """Convert PercentMAC object to Index object.
+
+        Args:
+            weight (int): Corresponding weight
+            ref_st (float): Reference station/axis. Selected station
+            around which all index values are calculated
+            c (int): Constant used as a denominator to convert
+            moment values into index values
+            k (int): Constant used as a plus value to avoid
+            negative index figures
+
+        Raises:
+            NotImplementedError: _description_
+        """
+        # TODO: implement this!
+        raise NotImplementedError(
+            "Conversion from PercentMAC to Index is not yet implemented"
+        )
--- src/tests/test_basic.py
+++ src/tests/test_basic.py
@@ -1,41 +1,232 @@
 import pytest
-from crjwb.basic import BasicCalc
+from crjwb.basic import PercentMAC, Index
 
 
 @pytest.fixture(scope="module")
-def basic_calc() -> BasicCalc:
-    """
-    Get BasicCalc with following parameters:
-
-    REF_STATION = 13.2
-
-    K_CONSTANT = 50
-
-    C_CONSTANT = 280
-
-    MACRC_LENGTH = 2.526
-
-    LEMAC_AT = 12.542
+def ref_st() -> float:
+    return 13.2
 
 
-    Yields:
-        BasicCalc: BasicCalc
-    """
-    return BasicCalc(13.2, 50, 280, 2.526, 12.542)
+@pytest.fixture(scope="module")
+def k() -> int:
+    return 50
+
+
+@pytest.fixture(scope="module")
+def c() -> int:
+    return 280
+
+
+@pytest.fixture(scope="module")
+def macrc_length() -> float:
+    return 2.526
+
+
+@pytest.fixture(scope="module")
+def lemac_at() -> float:
+    return 12.542
+
+
+# WBIndex tests
+
+
+def test_init_wbindex(ref_st: float, c: int, k: int):
+    idx = Index(50, 14500, ref_st, c, k)
+    assert idx.value == 50
+    assert idx.weight == 14500
+    assert idx.ref_st == ref_st
+    assert idx.c == c
+    assert idx.k == k
+
+
+@pytest.mark.parametrize(["c"], [(0,), (-1,)])
+def test_no_negative_or_zero_c(c):
+    with pytest.raises(ValueError, match="C constant should be greater than 0"):
+        Index(50, 14500, 10.0, c, 50)
+
+
+def test_no_negative_k():
+    with pytest.raises(ValueError, match="K constant should not be negative"):
+        Index(50, 14500, 10.0, 1, -1)
 
 
 @pytest.mark.parametrize(
-    "basic_calc, idx, weight, mac",
-    [
-        ("basic_calc", 29.84, 17843, 13.52),
-        ("basic_calc", 23.72, 21627, 12.58),
-        ("basic_calc", 23.83, 20354, 11.80),
-    ],
-    indirect=["basic_calc"],
+    ["idx_value", "weight", "c", "k"], [(10, 10, 1, 0), (48.5, 14500, 280, 50)]
 )
-def test_calc_macrc(basic_calc, idx, weight, mac):
-    """
-    Compare BasicCalc.calc_macrc() calculations results with \
-    real loadsheet data.
-    """
-    assert basic_calc.calc_macrc(idx, weight) == pytest.approx(mac, 1e-3)
+def test_moment_property(idx_value: float, weight: int, c: int, k: int):
+    idx = Index(idx_value, weight, 14.0, c, k)
+    assert idx.moment == (idx_value - k) * c
+
+
+def test_calc():
+    idx = Index.calc(100, 10, 0, 2, 50)
+    assert idx.value == 550
+    assert idx.moment == 1000
+
+
+@pytest.mark.parametrize(["ref_st", "c", "k"], [(5, 1, 0), (10, 2, 0), (10, 1, 2)])
+def test_validate_calc_ops_raises(ref_st: float, c: int, k: int):
+    a = Index(10, 10, 10, 1, 0)
+    b = Index(10, 10, ref_st, c, k)
+    with pytest.raises(
+        ValueError,
+        match="Calculations for Index operands with different "
+        "reference stations, C or K constants are not allowed.",
+    ):
+        a.__validate_calc_ops__(b)
+
+
+def test_validate_calc_ops_does_not_raise():
+    a = Index(5, 20, 10, 1, 0)
+    b = Index(2, 30, 10, 1, 0)
+    a.__validate_calc_ops__(b)
+
+
+def test_validate_comp_ops_raises():
+    a = Index(5, 20, 9, 1, 0)
+    b = Index(2, 30, 10, 1, 0)
+    with pytest.raises(
+        ValueError,
+        match="Cannot compare Index instances with different reference stations.",
+    ):
+        a.__validate_compare_ops__(b)
+
+
+def test_validate_comp_ops_does_not_raise():
+    a = Index(5, 20, 10, 1, 0)
+    b = Index(2, 30, 10, 1, 0)
+    a.__validate_compare_ops__(b)
+
+
+def test_add():
+    a = Index(5, 20, 10, 1, 0)
+    b = Index(2, 30, 10, 1, 0)
+    c = a + b
+    assert c.value == a.value + b.value
+    assert c.weight == a.weight + b.weight
+
+
+def test_sub():
+    a = Index(5, 20, 10, 1, 0)
+    b = Index(2, 30, 10, 1, 0)
+    c = a - b
+    assert c.value == a.value - b.value
+    assert c.weight == a.weight - b.weight
+
+
+def test_eq():
+    a = Index(5, 20, 10, 1, 0)
+    b = Index(5, 20, 10, 1, 0)
+    assert a == b
+
+
+def test_gt():
+    a = Index(10, 20, 30, 1, 0)
+    b = Index(9, 20, 30, 1, 0)
+    assert a > b
+
+
+def test_lt():
+    a = Index(9, 20, 30, 1, 0)
+    b = Index(10, 20, 30, 1, 0)
+    assert a < b
+
+
+def test_ge():
+    a = Index(10, 20, 30, 1, 0)
+    b = Index(10, 20, 30, 1, 0)
+    c = Index(9, 20, 30, 1, 0)
+    assert a >= b
+    assert a >= c
+
+
+def test_le():
+    a = Index(10, 20, 30, 1, 0)
+    b = Index(10, 20, 30, 1, 0)
+    c = Index(11, 20, 30, 1, 0)
+    assert a <= b
+    assert a <= c
+
+
+# TODO: PercentMAC tests
+
+
+def test_pctmac_init(lemac_at: float, macrc_length: float):
+    pctmac = PercentMAC(13.52, lemac_at, macrc_length)
+    assert pctmac.value == 13.52
+    assert pctmac.lemac_at == lemac_at
+    assert pctmac.macrc_length == macrc_length
+
+
+@pytest.mark.parametrize(
+    [
+        "idx_value",
+        "weight",
+        "pctmac_value",
+        "ref_st",
+        "c",
+        "k",
+        "lemac_at",
+        "macrc_length",
+    ],
+    [
+        (29.84, 17841, 13.52, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+        (23.72, 21627, 12.58, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+        (23.83, 20354, 11.80, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+    ],
+    indirect=["ref_st", "c", "k", "lemac_at", "macrc_length"],
+)
+def test_pctmac_from_idx(
+    idx_value: float,
+    weight: int,
+    pctmac_value: float,
+    ref_st: float,
+    c: int,
+    k: int,
+    lemac_at: float,
+    macrc_length: float,
+):
+    idx = Index(idx_value, weight, ref_st, c, k)
+    pctmac = PercentMAC.from_idx(idx, lemac_at, macrc_length)
+    assert pctmac.value == pytest.approx(pctmac_value, 1e-3)
+    assert pctmac.lemac_at == lemac_at
+    assert pctmac.macrc_length == macrc_length
+
+
+@pytest.mark.parametrize(
+    [
+        "idx_value",
+        "weight",
+        "pctmac_value",
+        "ref_st",
+        "c",
+        "k",
+        "lemac_at",
+        "macrc_length",
+    ],
+    [
+        (29.84, 17841, 13.52, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+        (23.72, 21627, 12.58, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+        (23.83, 20354, 11.80, "ref_st", "c", "k", "lemac_at", "macrc_length"),
+    ],
+    indirect=["ref_st", "c", "k", "lemac_at", "macrc_length"],
+)
+def test_pctmac_to_idx(
+    idx_value: float,
+    weight: int,
+    pctmac_value: float,
+    ref_st: float,
+    c: int,
+    k: int,
+    lemac_at: float,
+    macrc_length: float,
+):
+    pctmac = PercentMAC(pctmac_value, lemac_at, macrc_length)
+    exp_idx = Index(idx_value, weight, ref_st, c, k)
+    with pytest.raises(NotImplementedError):
+        idx = pctmac.to_idx(weight, ref_st, c, k)
+        assert idx.value == pytest.approx(exp_idx.value, 1e-3)
+        assert idx.weight == exp_idx.weight
+        assert idx.ref_st == exp_idx.ref_st
+        assert idx.c == exp_idx.c
+        assert idx.k == exp_idx.k
