d6399488876ad6d31664002c59cf0570a719d99b
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-30 00:05:15 +0300

    LimitLine now uses IndexConstants

--- src/tests/test_cglimits.py
+++ src/tests/test_cglimits.py
@@ -1,86 +1,118 @@
-from typing import List, Tuple
+from typing import List
 
 import pytest
-from wbkit.basic import Index, PercentMAC
-from wbkit.cglimits import LimitLine
+from wbkit.basic import Index, IndexConstants
+from wbkit.cglimits import LimitLine, CGLimits
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def ref_st() -> float:
     return 13.2
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def c() -> int:
     return 280
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def k() -> int:
     return 50
 
 
-@pytest.fixture(scope="module")
-def macrc_length() -> float:
-    return 2.526
-
-
-@pytest.fixture(scope="module")
-def lemac_at() -> float:
-    return 12.542
+@pytest.fixture
+def rck(ref_st: float, c: int, k: int) -> IndexConstants:
+    return IndexConstants(ref_st, c, k)
 
 
 @pytest.fixture
-def idx_list(ref_st: float, c: int, k: int) -> List[Index]:
-    return [
-        Index(x, 13500 + n * 1000, ref_st, c, k)
-        for n, x in enumerate(range(20, 70, 10))
-    ]
+def idx_list(rck: IndexConstants) -> List[Index]:
+    return [Index(x, 13500 + n * 1000, rck) for n, x in enumerate(range(20, 70, 10))]
 
 
 @pytest.fixture
-def pctmac_weight_pairs(
-    idx_list: List[Index], macrc_length: float, lemac_at: float
-) -> List[Tuple[int, PercentMAC]]:
-    return [
-        (x.weight, PercentMAC.from_idx(x, lemac_at, macrc_length)) for x in idx_list
-    ]
+def ll(idx_list: List[Index]) -> LimitLine:
+    return LimitLine.from_idxs(idx_list)
 
 
 @pytest.fixture(
     params=[(True, False, False), (False, True, False), (False, False, True)]
 )
-def invalid_idx_list(request, idx_list, ref_st, c, k):
+def invalid_idx_list(request, idx_list, ref_st: float, c: int, k: int) -> List[Index]:
     return idx_list + [
         Index(
             70,
             24000,
-            999 if request.param[0] else ref_st,
-            999 if request.param[1] else c,
-            999 if request.param[2] else k,
+            IndexConstants(
+                999 if request.param[0] else ref_st,
+                999 if request.param[1] else c,
+                999 if request.param[2] else k,
+            ),
         )
     ]
 
 
 @pytest.fixture
-def ll(idx_list) -> LimitLine:
-    return LimitLine.from_idxs(idx_list)
+def fwd_line(rck) -> LimitLine:
+    points = {
+        13608: 34.45,
+        14515: 33.22,
+        15190: 30.14,
+        19958: 22.82,
+    }
+    return LimitLine([(x[0], x[1]) for x in points.items()], rck)
+
+
+@pytest.fixture
+def aft_line(rck) -> LimitLine:
+    points = {
+        13608: 57.31,
+        15422: 58.28,
+        16329: 63.19,
+        19958: 66.12,
+    }
+    return LimitLine([(x[0], x[1]) for x in points.items()], rck)
+
+
+@pytest.fixture
+def intersecting_lines(rck) -> List[LimitLine]:
+    return [
+        LimitLine([(0, 0), (10, 10)], rck),
+        LimitLine([(10, 0), (0, 10)], rck),
+    ]
+
+
+@pytest.fixture
+def valid_idx(rck) -> Index:
+    return Index(50, 14500, rck)
+
+
+@pytest.fixture
+def violates_fwd_idx(rck) -> Index:
+    return Index(33, 14500, rck)
+
+
+@pytest.fixture
+def violates_aft_idx(rck) -> Index:
+    return Index(60, 15500, rck)
+
+
+@pytest.fixture
+def cglimits(rck, fwd_line, aft_line) -> CGLimits:
+    cglimits = CGLimits(rck, [("Zero Fuel", fwd_line, aft_line)])
+    return cglimits
 
 
 class TestLimitLine:
     @staticmethod
-    def test_init(ref_st: float, c: int, k: int):
-        ll = LimitLine([(1, 2), (2, 3)], ref_st, c, k)
-        assert ll.ref_st == ref_st
-        assert ll.c == c
-        assert ll.k == k
+    def test_init(rck: IndexConstants):
+        ll = LimitLine([(1, 2), (2, 3)], rck)
+        assert ll.rck == rck
 
     @staticmethod
-    def test_init_from_idxs(idx_list: List[Index], ref_st: float, c: int, k: int):
+    def test_from_idxs(idx_list: List[Index], rck: IndexConstants):
         ll = LimitLine.from_idxs(idx_list)
-        assert ll.ref_st == ref_st
-        assert ll.c == c
-        assert ll.k == k
+        assert ll.rck == rck
 
     @staticmethod
     def test_invalid_idx_seq_raises(invalid_idx_list: List[Index]):
@@ -88,14 +120,5 @@ class TestLimitLine:
             LimitLine.from_idxs(invalid_idx_list)
 
     @staticmethod
-    def test_init_from_pctmac_pairs(
-        pctmac_weight_pairs: List[Tuple[int, PercentMAC]], ref_st: float, c: int, k: int
-    ):
-        ll = LimitLine.from_wght_pctmac_pairs(pctmac_weight_pairs, ref_st, c, k)
-        assert ll.ref_st == ref_st
-        assert ll.c == c
-        assert ll.k == k
-
-    @staticmethod
-    def test_get_limit_idx(ll: LimitLine, ref_st: float, c: int, k: int):
-        assert ll.get_limit_idx(13500) == Index(20, 13500, ref_st, c, k)
+    def test_get_limit_idx(ll: LimitLine, rck):
+        assert ll.get_limit_idx(13500) == Index(20, 13500, rck)
--- src/wbkit/cglimits.py
+++ src/wbkit/cglimits.py
@@ -1,6 +1,6 @@
-from typing import Sequence, Tuple, Union
+from typing import Dict, Hashable, Optional, Sequence, Tuple, Union
 
-from wbkit.basic import Index, PercentMAC
+from wbkit.basic import Index, IndexConstants
 from wbkit.interpolables import Interpolable
 
 
@@ -10,9 +10,7 @@ class LimitLine(Interpolable):
     def __init__(
         self,
         points: Sequence[Tuple[Union[int, float], Union[int, float]]],
-        ref_st: float,
-        c: int,
-        k: int,
+        rck: IndexConstants,
     ) -> None:
         """Create LimitLine object.
 
@@ -26,12 +24,10 @@ class LimitLine(Interpolable):
             negative index figures
         """
         super().__init__(points)
-        self.ref_st = ref_st
-        self.c = c
-        self.k = k
+        self.rck = rck
 
-    @staticmethod
-    def from_idxs(indices: Sequence[Index]):
+    @classmethod
+    def from_idxs(cls, indices: Sequence[Index]):
         """Initialize LimitLine object using sequence of Index objects.
         All Index objects should have the same ref_st, c and k attributes.
 
@@ -49,44 +45,57 @@ class LimitLine(Interpolable):
             raise ValueError(
                 "at least two indices should be provided to create LimitLine"
             )
-        rck = {(x.ref_st, x.c, x.k) for x in indices}
-        if len(rck) > 1:
+        rck_set = {(x.rck) for x in indices}
+        if len(rck_set) > 1:
             raise ValueError(
                 "unable to initialize LimitLine from Index sequence "
                 "objects having different ref_st, c or k attributes:\n"
                 + "\n".join(
-                    [f"{n}: REF_ST={x[0]} C={x[1]} K={x[2]}" for n, x in enumerate(rck)]
+                    [
+                        f"{n}: REF_ST={x.ref_st} C={x.c} K={x.k}"
+                        for n, x in enumerate(rck_set)
+                    ]
                 )
             )
-        ref_st, c, k = rck.pop()
-        return LimitLine(tuple((x.weight, x.value) for x in indices), ref_st, c, k)
-
-    @staticmethod
-    def from_wght_pctmac_pairs(
-        wght_pctmac_pairs: Sequence[Tuple[int, PercentMAC]],
-        ref_st: float,
-        c: int,
-        k: int,
-    ):
-        """Initialize LimitLine object using sequence of Index objects converted
-        from given weight-PercentMAC pairs.
-
-        Args:
-            wght_pctmac_pairs (Sequence[Tuple[int, PercentMAC]]): weight-PercentMAC
-            pairs
-            ref_st (float): Reference station/axis. Selected station
-            around which all index values are calculated
-            c (int): Constant used as a denominator to convert
-            moment values into index values
-            k (int): Constant used as a plus value to avoid
-            negative index figures
-
-        Returns:
-            LimitLine: LimitLine object
-        """
-        return LimitLine.from_idxs(
-            tuple(x[1].to_idx(x[0], ref_st, c, k) for x in wght_pctmac_pairs)
-        )
+        rck = rck_set.pop()
+        return cls(tuple((x.weight, x.value) for x in indices), rck)
 
     def get_limit_idx(self, weight: int) -> Index:
-        return Index(self.interp(weight), weight, self.ref_st, self.c, self.k)
+        return Index(self.interp(weight), weight, self.rck)
+
+
+class CGLimits:
+    def __init__(
+        self,
+        rck: Optional[IndexConstants],
+        limits: Optional[Sequence[Tuple[Hashable, LimitLine, LimitLine]]] = None,
+    ) -> None:
+        self.rck = rck
+        self.__phases__: Dict[Hashable, Tuple[LimitLine, LimitLine]] = dict()
+        if limits:
+            for limit in limits:
+                self.add_limits(limit[0], limit[1], limit[2])
+
+    def add_limits(self, phase: Hashable, fwd_line: LimitLine, aft_line: LimitLine):
+        if not fwd_line.rck == aft_line.rck:
+            raise ValueError(
+                "both fwd and aft LimitLine objects should have "
+                "the same ref_st, k and c attribute values"
+            )
+        if self.rck is None:
+            self.rck = fwd_line.rck
+        if len({self.rck, fwd_line.rck}) > 1:
+            raise ValueError(
+                "CGLimits object cannot have LimitLines with "
+                "different IndexConstants"
+            )
+        if fwd_line.intersects(aft_line):
+            raise ValueError("forward and aft limits should not intersect")
+        self.__phases__.update({phase: (fwd_line, aft_line)})
+
+    def idx_in_limits(self, phase: Hashable, idx: Index) -> Tuple[bool, bool]:
+        if phase not in self.__phases__:
+            raise ValueError(f'limits for phase "{phase}" are not defined')
+        return not idx < self.__phases__[phase][0].get_limit_idx(
+            idx.weight
+        ), not idx > self.__phases__[phase][1].get_limit_idx(idx.weight)
