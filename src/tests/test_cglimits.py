d8fd5110a5c3e8e4c6b12da4196752abb6d4288f
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-29 13:24:07 +0300

    implemented from_wght_pctmac_pairs static method

--- src/tests/test_cglimits.py
+++ src/tests/test_cglimits.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Tuple
 import pytest
-from wbkit.basic import Index
+from wbkit.basic import Index, PercentMAC
 from wbkit.cglimits import LimitLine
 
 
@@ -10,20 +10,39 @@ def ref_st() -> float:
 
 
 @pytest.fixture(scope="module")
+def c() -> int:
+    return 280
+
+
+@pytest.fixture(scope="module")
 def k() -> int:
     return 50
 
 
 @pytest.fixture(scope="module")
-def c() -> int:
-    return 280
+def macrc_length() -> float:
+    return 2.526
+
+
+@pytest.fixture(scope="module")
+def lemac_at() -> float:
+    return 12.542
 
 
 @pytest.fixture
-def idx_list(ref_st: float, c: int, k: int):
+def idx_list(ref_st: float, c: int, k: int) -> List[Index]:
     return [
         Index(x, 13500 + n * 1000, ref_st, c, k)
         for n, x in enumerate(range(20, 70, 10))
+    ]
+
+
+@pytest.fixture
+def pctmac_weight_pairs(
+    idx_list: List[Index], macrc_length: float, lemac_at: float
+) -> List[Tuple[int, PercentMAC]]:
+    return [
+        (x.weight, PercentMAC.from_idx(x, lemac_at, macrc_length)) for x in idx_list
     ]
 
 
@@ -44,20 +63,20 @@ def invalid_idx_list(request, idx_list, ref_st, c, k):
 
 @pytest.fixture
 def ll(idx_list) -> LimitLine:
-    return LimitLine.from_idx_seq(idx_list)
+    return LimitLine.from_idxs(idx_list)
 
 
 class TestLimitLine:
     @staticmethod
     def test_init(ref_st: float, c: int, k: int):
-        ll = LimitLine([(1, 2), (2, 3)], ref_st, k, c)
+        ll = LimitLine([(1, 2), (2, 3)], ref_st, c, k)
         assert ll.ref_st == ref_st
         assert ll.c == c
         assert ll.k == k
 
     @staticmethod
-    def test_init_from_idx_seq(idx_list: List[Index], ref_st: float, c: int, k: int):
-        ll = LimitLine.from_idx_seq(idx_list)
+    def test_init_from_idxs(idx_list: List[Index], ref_st: float, c: int, k: int):
+        ll = LimitLine.from_idxs(idx_list)
         assert ll.ref_st == ref_st
         assert ll.c == c
         assert ll.k == k
@@ -65,7 +84,16 @@ class TestLimitLine:
     @staticmethod
     def test_invalid_idx_seq_raises(invalid_idx_list: List[Index]):
         with pytest.raises(ValueError):
-            LimitLine.from_idx_seq(invalid_idx_list)
+            LimitLine.from_idxs(invalid_idx_list)
+
+    @staticmethod
+    def test_init_from_pctmac_pairs(
+        pctmac_weight_pairs: List[Tuple[int, PercentMAC]], ref_st: float, c: int, k: int
+    ):
+        ll = LimitLine.from_wght_pctmac_pairs(pctmac_weight_pairs, ref_st, c, k)
+        assert ll.ref_st == ref_st
+        assert ll.c == c
+        assert ll.k == k
 
     @staticmethod
     def test_get_limit_idx(ll: LimitLine, ref_st: float, c: int, k: int):
--- src/wbkit/cglimits.py
+++ src/wbkit/cglimits.py
@@ -1,38 +1,91 @@
 from typing import Sequence, Tuple, Union
-from wbkit.basic import Index
+from wbkit.basic import Index, PercentMAC
 from wbkit.interpolables import Interpolable
 
 
 class LimitLine(Interpolable):
+    """Object representing CG Limit line."""
+
     def __init__(
         self,
         points: Sequence[Tuple[Union[int, float], Union[int, float]]],
         ref_st: float,
-        k: int,
         c: int,
+        k: int,
     ) -> None:
+        """Create LimitLine object.
+
+        Args:
+            points (Sequence[Tuple[int |float, int | float]]]): weight-index pairs
+            ref_st (float): Reference station/axis. Selected station
+            around which all index values are calculated
+            c (int): Constant used as a denominator to convert
+            moment values into index values
+            k (int): Constant used as a plus value to avoid
+            negative index figures
+        """
         super().__init__(points)
         self.ref_st = ref_st
-        self.k = k
         self.c = c
+        self.k = k
 
     @staticmethod
-    def from_idx_seq(indices: Sequence[Index]):
+    def from_idxs(indices: Sequence[Index]):
+        """Initialize LimitLine object using sequence of Index objects.
+        All Index objects should have the same ref_st, c and k attributes.
+
+        Args:
+            indices (Sequence[Index]): sequence of Index objects
+
+        Raises:
+            ValueError: if sequence has less than two Index objects
+            ValueError: if Index objects have different ref_st, c and k attributes
+
+        Returns:
+            LimitLine: LimitLine object
+        """
         if len(indices) < 2:
             raise ValueError(
                 "at least two indices should be provided to create LimitLine"
             )
-        rkc = {(x.ref_st, x.k, x.c) for x in indices}
-        if len(rkc) > 1:
+        rck = {(x.ref_st, x.c, x.k) for x in indices}
+        if len(rck) > 1:
             raise ValueError(
                 "unable to initialize LimitLine from Index sequence "
                 "objects having different ref_st, c or k attributes:\n"
                 + "\n".join(
-                    [f"{n}: REF_ST={x[0]} K={x[1]} C={x[2]}" for n, x in enumerate(rkc)]
+                    [f"{n}: REF_ST={x[0]} C={x[1]} K={x[2]}" for n, x in enumerate(rck)]
                 )
             )
-        ref_st, k, c = rkc.pop()
-        return LimitLine([(x.weight, x.value) for x in indices], ref_st, k, c)
+        ref_st, c, k = rck.pop()
+        return LimitLine(tuple((x.weight, x.value) for x in indices), ref_st, c, k)
+
+    @staticmethod
+    def from_wght_pctmac_pairs(
+        wght_pctmac_pairs: Sequence[Tuple[int, PercentMAC]],
+        ref_st: float,
+        c: int,
+        k: int,
+    ):
+        """Initialize LimitLine object using sequence of Index objects converted
+        from given weight-PercentMAC pairs.
+
+        Args:
+            wght_pctmac_pairs (Sequence[Tuple[int, PercentMAC]]): weight-PercentMAC
+            pairs
+            ref_st (float): Reference station/axis. Selected station
+            around which all index values are calculated
+            c (int): Constant used as a denominator to convert
+            moment values into index values
+            k (int): Constant used as a plus value to avoid
+            negative index figures
+
+        Returns:
+            LimitLine: LimitLine object
+        """
+        return LimitLine.from_idxs(
+            tuple(x[1].to_idx(x[0], ref_st, c, k) for x in wght_pctmac_pairs)
+        )
 
     def get_limit_idx(self, weight: int) -> Index:
         return Index(self.interp(weight), weight, self.ref_st, self.c, self.k)
