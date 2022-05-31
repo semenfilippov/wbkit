76b753fe193a0334135601b3536ef6cde4807914
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-05-31 23:15:53 +0300

    extract fixtures to conftest

--- src/tests/test_cglimits.py
+++ src/tests/test_cglimits.py
@@ -4,65 +4,30 @@ from wbkit.cglimits import CGLimits
 from wbkit.interpolables import Interpolable
 
 
-@pytest.fixture
-def fwd_dict() -> dict:
-    return {
-        13608: 34.45,
-        14515: 33.22,
-        15190: 30.14,
-        19958: 22.82,
-    }
-
-
-@pytest.fixture
-def aft_dict() -> dict:
-    return {
-        13608: 57.31,
-        15422: 58.28,
-        16329: 63.19,
-        19958: 66.12,
-    }
-
-
-@pytest.fixture
-def fwd_line(fwd_dict) -> Interpolable:
-    return Interpolable([(x, y) for x, y in fwd_dict.items()])
-
-
-@pytest.fixture
-def aft_line(aft_dict) -> Interpolable:
-    return Interpolable([(x, y) for x, y in aft_dict.items()])
-
-
-@pytest.fixture
-def cglimits(fwd_line, aft_line) -> CGLimits:
-    return CGLimits(fwd_line, aft_line)
-
-
 @pytest.fixture(params=["short_min", "short_max"])
-def invalid_fwd_line(request, fwd_dict) -> Interpolable:
+def invalid_fwd_line(request, fwd_zfw_dict) -> Interpolable:
     if request.param == "short_min":
-        _, *rest_t = fwd_dict.items()
+        _, *rest_t = fwd_zfw_dict.items()
         rest = dict(rest_t)
-        return Interpolable([(x, y) for x, y in rest.items()])
+        return Interpolable(*rest.items())
     elif request.param == "short_max":
-        *rest_t, _ = fwd_dict.items()
+        *rest_t, _ = fwd_zfw_dict.items()
         rest = dict(rest_t)
-        return Interpolable([(x, y) for x, y in rest.items()])
+        return Interpolable(*rest.items())
     else:
         raise ValueError("invalid_fwd_line internal fault")
 
 
 @pytest.fixture(params=["short_min", "short_max"])
-def invalid_aft_line(request, aft_dict) -> Interpolable:
+def invalid_aft_line(request, aft_zfw_dict) -> Interpolable:
     if request.param == "short_min":
-        _, *rest_t = aft_dict.items()
+        _, *rest_t = aft_zfw_dict.items()
         rest = dict(rest_t)
-        return Interpolable([(x, y) for x, y in rest.items()])
+        return Interpolable(*rest.items())
     elif request.param == "short_max":
-        *rest_t, _ = aft_dict.items()
+        *rest_t, _ = aft_zfw_dict.items()
         rest = dict(rest_t)
-        return Interpolable([(x, y) for x, y in rest.items()])
+        return Interpolable(*rest.items())
     else:
         raise ValueError("invalid_aft_line internal fault")
 
@@ -82,34 +47,34 @@ def bad_aft_idx(rck) -> Index:
     return Index(59, 15400, rck)
 
 
-def test_diff_fwd(invalid_fwd_line, aft_line):
+def test_diff_fwd(invalid_fwd_line, zfw_aft_line):
     with pytest.raises(
         ValueError,
         match="fwd_line and aft_line min and max weights should be equal",
     ):
-        CGLimits(invalid_fwd_line, aft_line)
+        CGLimits(invalid_fwd_line, zfw_aft_line)
 
 
-def test_diff_aft(fwd_line, invalid_aft_line):
+def test_diff_aft(zfw_fwd_line, invalid_aft_line):
     with pytest.raises(
         ValueError,
         match="fwd_line and aft_line min and max weights should be equal",
     ):
-        CGLimits(fwd_line, invalid_aft_line)
+        CGLimits(zfw_fwd_line, invalid_aft_line)
 
 
-def test_wrong_order(fwd_line, aft_line):
+def test_wrong_order(zfw_fwd_line, zfw_aft_line):
     with pytest.raises(ValueError, match="make sure order of lines is fwd, aft"):
-        CGLimits(aft_line, fwd_line)
+        CGLimits(zfw_aft_line, zfw_fwd_line)
 
 
-def test_good_idx(cglimits, good_idx):
-    assert good_idx in cglimits
+def test_good_idx(zfw_cglimits, good_idx):
+    assert good_idx in zfw_cglimits
 
 
-def test_bad_fwd_idx(cglimits, bad_fwd_idx):
-    assert bad_fwd_idx not in cglimits
+def test_bad_fwd_idx(zfw_cglimits, bad_fwd_idx):
+    assert bad_fwd_idx not in zfw_cglimits
 
 
-def test_bad_aft_idx(cglimits, bad_aft_idx):
-    assert bad_aft_idx not in cglimits
+def test_bad_aft_idx(zfw_cglimits, bad_aft_idx):
+    assert bad_aft_idx not in zfw_cglimits
