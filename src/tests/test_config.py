2516127b0f86aa71b8b38b85dea0e9a5869a3c01
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-06-01 22:44:29 +0300

    introduced Passenger and Cargo locations configs

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -1,4 +1,10 @@
-from wbkit.config import FuelEffect, StabRange, WBConfig
+from wbkit.config import (
+    FuelEffect,
+    StabRange,
+    WBConfig,
+    PassengerLocation,
+    CargoLocation,
+)
 
 
 def test_config(zfw_cglimits):
@@ -6,10 +12,17 @@ def test_config(zfw_cglimits):
         zf = zfw_cglimits
 
     class CRJ200(CRJ):
-        a = StabRange([(8.8, 8.15), (35, 4)])
+        all = StabRange([(8.8, 8.15), (35, 4)])
         fuel = FuelEffect([(0, 0), (6000, -17)])
+        a = PassengerLocation(-0.1013, 16)
+        aft = CargoLocation(0.16, 1500)
 
     crj = CRJ200()
     assert crj.cglimits["zf"] == zfw_cglimits
-    assert crj.stab["a"][8.8] == 8.15
+    assert crj.stabs["all"][8.8] == 8.15
+    assert crj.stab[35] == 4
     assert crj.fuel[6000] == -17
+    assert crj.paxlocs["a"].capacity == 16
+    assert crj.paxlocs["a"].influence == -0.1013
+    assert crj.cargolocs["aft"].influence == 0.16
+    assert crj.cargolocs["aft"].max_weight == 1500
--- src/wbkit/config.py
+++ src/wbkit/config.py
@@ -1,6 +1,26 @@
-from typing import Any, Dict
-from wbkit.interpolables import Interpolable
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional
+
 from wbkit.cglimits import CGLimits
+from wbkit.interpolables import Interpolable
+
+
+class Location(ABC):
+    @abstractmethod
+    def __init__(self, influence: float) -> None:
+        self.influence = influence
+
+
+class PassengerLocation(Location):
+    def __init__(self, influence: float, capacity: int) -> None:
+        super().__init__(influence)
+        self.capacity = capacity
+
+
+class CargoLocation(Location):
+    def __init__(self, influence: float, max_weight: int) -> None:
+        super().__init__(influence)
+        self.max_weight = max_weight
 
 
 class FuelEffect(Interpolable):
@@ -12,34 +32,44 @@ class StabRange(Interpolable):
 
 
 class WBConfigBase(type):
+    cargolocs: Dict[str, CargoLocation]
     cglimits: Dict[str, CGLimits]
-    fuel: FuelEffect
-    stab: Dict[str, StabRange]
+    fuel: Optional[FuelEffect]
+    paxlocs: Dict[str, PassengerLocation]
+    stabs: Dict[str, StabRange]
 
     def __new__(cls, name, bases, attrs: Dict[str, Any]):
         if name is None:
             return None
 
         new_attrs: Dict[str, Any] = {}
-        new_attrs["stab"] = {}
-        new_attrs["fuel"] = None
+        new_attrs["cargolocs"] = {}
         new_attrs["cglimits"] = {}
+        new_attrs["fuel"] = None
+        new_attrs["paxlocs"] = {}
+        new_attrs["stabs"] = {}
 
         for baseclass in bases:  # not sure if this is correct, but it works
             if isinstance(baseclass, cls):
                 new_attrs["cglimits"].update(baseclass.cglimits)
                 new_attrs["fuel"] = baseclass.fuel
-                new_attrs["stab"].update(baseclass.stab)
+                new_attrs["stabs"].update(baseclass.stabs)
 
         for k, v in attrs.items():
+            if isinstance(v, CargoLocation):
+                new_attrs["cargolocs"][k] = v
+                continue
             if isinstance(v, CGLimits):
                 new_attrs["cglimits"][k] = v
                 continue
             if isinstance(v, FuelEffect):
                 new_attrs["fuel"] = v
                 continue
+            if isinstance(v, PassengerLocation):
+                new_attrs["paxlocs"][k] = v
+                continue
             if isinstance(v, StabRange):
-                new_attrs["stab"][k] = v
+                new_attrs["stabs"][k] = v
                 continue
             new_attrs[k] = v
 
@@ -47,6 +77,14 @@ class WBConfigBase(type):
 
 
 class WBConfig(metaclass=WBConfigBase):
+    cargolocs: Dict[str, CargoLocation]
     cglimits: Dict[str, CGLimits]
-    fuel: FuelEffect
-    stab: Dict[str, StabRange]
+    fuel: Optional[FuelEffect]
+    paxlocs: Dict[str, PassengerLocation]
+    stabs: Dict[str, StabRange]
+
+    @property
+    def stab(self) -> Optional[StabRange]:
+        if len(self.stabs) == 1:
+            return next(iter(self.stabs.values()))
+        return None
