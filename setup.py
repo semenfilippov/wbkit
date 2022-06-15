c76dc9e76c9b8cbb0e8de227067357ff0fa759b2
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-06-15 11:57:16 +0300

    moved from setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wbkit",
+    version="0.0.2",
+    author="semenfilippov",
+    author_email="semenfilippov@gmail.com",
+    description="Aircraft weight and balance framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/semenfilippov/wbkit",
+    project_urls={
+        "Bug Tracker": "https://github.com/semenfilippov/wbkit/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src", exclude=["tests"]),
+    python_requires=">=3.10",
+    install_requires=["numpy>=1.22,<2"],
+)
