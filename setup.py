615249ef0eac15500e8a6765ce1e0827699947d2
Semen Filippov <89679660+semenfilippov@users.noreply.github.com>
2022-07-17 15:41:56 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wbkit",
-    version="0.0.6",
+    version="0.0.7",
     author="semenfilippov",
     author_email="semenfilippov@gmail.com",
     description="Aircraft weight and balance framework",
